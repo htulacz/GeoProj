53ab24ee4e62e5e00144138cbd189dac7c4de577
Hubert Tułacz <htulacz@student.agh.edu.pl>
2023-12-31 04:30:02 +0100

    naprawienie nowych testów + skalowanie visualizera

--- visualizer.py
+++ visualizer.py
@@ -1,21 +1,33 @@
 import matplotlib.pyplot as plt
-
-def draw_halfedge_structure(halfedge,mesh):
+def draw_halfedge_structure(halfedge, mesh):
     plt.figure(figsize=(8, 8))
     i = 0
     j = 0
-    c = ['red','magenta','green','orange','purple','black']
+    c = ['red', 'magenta', 'green', 'orange', 'purple', 'black']
 
-    offset = 0.015  
-    shortening = 0.1  
+    # Obliczenie średniej długości krawędzi
+    lengths = []
     for section in mesh:
-        plt.plot([section[0][0],section[1][0]],[section[0][1],section[1][1]],color='blue')
-    for h in halfedge:
-        start = h.vertex.cords  
-        end = h.next.vertex.cords  
+        dx = section[1][0] - section[0][0]
+        dy = section[1][1] - section[0][1]
+        length = (dx ** 2 + dy ** 2) ** 0.5
+        lengths.append(length)
+    average_length = sum(lengths) / len(lengths)
 
-        dx = start[0] - end[0]  
-        dy = start[1] - end[1]  
+    # Dostosowanie offset i shortening
+    scale_factor = average_length / 5  # Można dostosować według potrzeb
+    offset = 0.1 * scale_factor
+    shortening = 0.1 * scale_factor
+
+    for section in mesh:
+        plt.plot([section[0][0], section[1][0]], [section[0][1], section[1][1]], color='blue')
+    
+    for h in halfedge:
+        start = h.vertex.cords
+        end = h.next.vertex.cords
+
+        dx = start[0] - end[0]
+        dy = start[1] - end[1]
 
         length = (dx ** 2 + dy ** 2) ** 0.5
         dx /= length
@@ -27,7 +39,7 @@ def draw_halfedge_structure(halfedge,mesh):
         end = (start[0] + (end[0] - start[0]) * (1 - shortening), start[1] + (end[1] - start[1]) * (1 - shortening))
         start = (start[0] + (end[0] - start[0]) * (1 - shortening), start[1] + (end[1] - start[1]) * (1 - shortening))
 
-        plt.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1], head_width=0.01, head_length=0.1, fc=c[j], ec=c[j])
+        plt.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1], head_width=offset, head_length=offset, fc=c[j], ec=c[j])
 
         i += 1
         j = (i // 3) % 6
